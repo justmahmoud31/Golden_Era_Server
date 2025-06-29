592bd788efd21c94a50a527355102a3575b699ee
justmahmoud31 <justmahmoud3152003@gmail.com>
2025-06-29 19:42:16 +0300

    Increased the server capcity

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ app.use(cors({
 }));
 app.use(helmet());
 app.use(morgan("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
